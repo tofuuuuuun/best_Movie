7551080bafa5f6cc832e0b35a966c70021abe7f3
hirano <hirakenlp21@gmail.com>
2024-11-09 09:22:21 +0900

    映画一覧表示エリアを作成

---
+++ app_Movie/frontend/src/components/MoviePosterList.tsx
@@ -0,0 +1,33 @@
+type Movie = {
+    id: string;
+    albumArt: string;
+    albumName: string;
+    albumArtist: string;
+}
+type AlbumArtListProps = {
+    isSelectStart: boolean;
+    moviePosterList: Movie[];
+    deleteAlbum: (id: string) => void;
+}
+
+export const MoviePosterList = (props: AlbumArtListProps) => {
+    const { moviePosterList, deleteAlbum } = props;
+    return (
+        <>
+            {moviePosterList.length != 0 && (
+                <div className='l-albumList l-common'>
+                    <ul className='albumArtList' id='target'>
+                        {moviePosterList.map((album, index) => (
+                            <li className='albumListItem action' id={album.id} key={index} >
+                                <img className='l-albumArt m-bottom-05em' src={album.albumArt} />
+                                <span className='selectName'>{album.albumName}</span>
+                                <span className='selectAlbumArtist'>{album.albumArtist}</span>
+                                <span className='albumRemove' onClick={() => deleteAlbum(album.id)}><span className='icon-close'></span></span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </>
+    )
+}
