6267fbed93ba1acaee20c4e6096de0285275ce8b
hirano <hirakenlp21@gmail.com>
2025-01-05 16:01:54 +0900

    映画ポスターの表示処理を改善し、モーダルの位置を調整

--- app_Movie/frontend/src/components/Modal/ResponseMovies.tsx
+++ app_Movie/frontend/src/components/Modal/ResponseMovies.tsx
@@ -1,14 +1,17 @@
 import { ResultCheckboxButton } from './ResultCheckButton';
 import { ResponseMoviesProps } from '../../../public/types';
+import NO_IMAGE from '../../../public/images/no_image.png';
+
+const BASE_URL = 'https://image.tmdb.org/t/p/w500/';
 
 export const ResponseMovies = (props: ResponseMoviesProps) => {
     const { toggleAlbum, responseMovies, moviePosterList } = props;
-    const BASE_URL = 'https://image.tmdb.org/t/p/w500/';
+    const setMoviePosterImage = (poster: string) => poster ? `${BASE_URL}${poster}` : NO_IMAGE;
     return (
         <ul className='modalList'>
             {responseMovies.map((movie, index) => (
                 <li className={`movieItems ${index === 0 ? 'first-item' : ''}`} key={index} >
-                    <img className='moviePoster' src={`${BASE_URL}${movie.poster_path ?? ''}`} loading='lazy' />
+                    <img className='moviePoster' src={setMoviePosterImage(movie.poster_path)} loading='lazy' />
                     <div className='l-movieInfo'>
                         <span className='movieTitle font-wb'>{movie.title}</span>
                     </div>
